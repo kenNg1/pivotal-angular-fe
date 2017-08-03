76f85ba6618c3c404821db565aac2e2342ccf976
kenNg1 <kng1230@hotmail.com>
2017-08-03 21:41:29 +0800

    filtering works

--- src/app/search/search-results.component.ts
+++ src/app/search/search-results.component.ts
@@ -9,6 +9,17 @@ import { EventService } from '../shared/event.service';
 })
 export class SearchResultsComponent implements OnInit {
   events:IEvent[]
+  bananas = this.events
+  sortBy:string = 'date'
+
+  sortDate(sort:string){
+    this.sortBy === 'date'
+    this.events = this.events.sort(sortByDateAsc)
+  }
+
+  sortPrice(sort:string){
+    this.events = this.events.sort(sortByPriceAsc)
+  }
 
   constructor(private eventService:EventService) { }
 
@@ -16,5 +27,18 @@ export class SearchResultsComponent implements OnInit {
     this.events = this.eventService.getEvents().map(events => events)
     return this.events
   }
+  
+}
 
+function sortByDateAsc(e1:IEvent, e2:IEvent){
+  if(e1.date > e2.date) return 1
+  else if (e1.date === e2.date) return 0
+  else return -1
+}
+
+function sortByPriceAsc(e1:IEvent, e2:IEvent){
+  console.log('ken')
+  if(e1.price > e2.price) return 1
+  else if (e1.price === e2.price) return 0
+  else return -1
 }
