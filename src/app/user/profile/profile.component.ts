5d42e60c56572ef6a291801cd0886f75391102d0
kenNg1 <kng1230@hotmail.com>
2017-09-04 13:07:56 +0800

    compare changes in CSS files

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,11 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, state, style,animate,transition, trigger, keyframes } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../authentication.service'
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
+  animations: [
+    trigger('carduserprofile', [
+        state('*', style({
+            '-ms-transform': 'translate3D(0px, 0px, 0px)',
+            '-webkit-transform': 'translate3D(0px, 0px, 0px)',
+            '-moz-transform': 'translate3D(0px, 0px, 0px)',
+            '-o-transform':'translate3D(0px, 0px, 0px)',
+            transform:'translate3D(0px, 0px, 0px)',
+            opacity: 1
+        })),
+        transition('void => *', [
+            style({opacity: 0,
+                '-ms-transform': 'translate3D(0px, 150px, 0px)',
+                '-webkit-transform': 'translate3D(0px, 150px, 0px)',
+                '-moz-transform': 'translate3D(0px, 150px, 0px)',
+                '-o-transform':'translate3D(0px, 150px, 0px)',
+                transform:'translate3D(0px, 150px, 0px)',
+            }),
+            animate('0.3s 0s ease-out'),
+        ])
+    ]),
+    trigger('cardprofile', [
+        state('*', style({
+            '-ms-transform': 'translate3D(0px, 0px, 0px)',
+            '-webkit-transform': 'translate3D(0px, 0px, 0px)',
+            '-moz-transform': 'translate3D(0px, 0px, 0px)',
+            '-o-transform':'translate3D(0px, 0px, 0px)',
+            transform:'translate3D(0px, 0px, 0px)',
+            opacity: 1})),
+            transition('void => *', [
+                style({opacity: 0,
+                    '-ms-transform': 'translate3D(0px, 150px, 0px)',
+                    '-webkit-transform': 'translate3D(0px, 150px, 0px)',
+                    '-moz-transform': 'translate3D(0px, 150px, 0px)',
+                    '-o-transform':'translate3D(0px, 150px, 0px)',
+                    transform:'translate3D(0px, 150px, 0px)',
+                }),
+                animate('0.3s 0.25s ease-out')
+            ])
+        ])
+    ]
 })
 export class ProfileComponent implements OnInit {
 
