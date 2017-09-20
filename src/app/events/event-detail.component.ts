a5a07cc694188e2bd0e195b9d29c946f5ed179fd
Kevin <cho.kevin@hotmail.com>
2017-09-21 01:36:08 +0800

    event search cleaned up

--- src/app/events/event-detail.component.ts
+++ src/app/events/event-detail.component.ts
@@ -87,7 +87,7 @@ export class EventDetailComponent implements OnInit, OnDestroy {
       this.districts = districts;});
   }
 
-  ngOnDestroy() {
+  ngOnDestroy() { 
     this.subscription.unsubscribe();
   }
 
